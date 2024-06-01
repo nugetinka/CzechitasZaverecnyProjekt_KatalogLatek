fca5932403d2c3445fbbbf2904188db51f376a49
Kristýna Sekničková <117662870+Nugetinka@users.noreply.github.com>
2024-06-01 11:24:08 +0200

    Add project files.

---
+++ ZaverecnyProjekt_KatalogLatek/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ZaverecnyProjekt_KatalogLatek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
