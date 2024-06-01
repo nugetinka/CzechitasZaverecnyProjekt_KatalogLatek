325c6196b526b2700840a65df94b375223fd9bba
Kristýna Sekničková <117662870+Nugetinka@users.noreply.github.com>
2024-06-01 11:34:35 +0200

    Vytvoření Projektu

--- ZaverecnyProjekt_KatalogLatek/Program.cs
+++ ZaverecnyProjekt_KatalogLatek/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Tady bude vznikat můj závěrený projekt z C# 2 - Czechitas. :)");
         }
     }
 }
