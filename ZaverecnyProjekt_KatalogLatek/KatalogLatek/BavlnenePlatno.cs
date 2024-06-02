ae91f90ec6098b3126f7492f76e0cf29cc72799d
Kristýna Sekničková <117662870+Nugetinka@users.noreply.github.com>
2024-06-02 22:54:07 +0200

    Závěrečný projekt - první vlaštovka

---
+++ ZaverecnyProjekt_KatalogLatek/KatalogLatek/BavlnenePlatno.cs
@@ -0,0 +1,20 @@
+﻿namespace ZaverecnyProjekt_KatalogLatek.KatalogLatek
+{
+    public class BavlnenePlatno : Latka
+    {
+        public int _srazlivost { get; private set; }
+
+        public BavlnenePlatno(int id, string nazev, string barva, string kategorie, string slozeni, double gramaz, double cena, double zasoba, bool certifikat, int srazlivost)
+            : base(id, nazev, barva, kategorie, slozeni, gramaz, cena, zasoba, certifikat)
+        {
+            _srazlivost = srazlivost;
+        }
+
+        public override void VypisInformaceOLatce()
+        {
+            base.VypisInformaceOLatce();
+            Console.WriteLine($"Srážlivost: {_srazlivost} %");
+            Console.WriteLine();
+        }
+    }
+}
--- ZaverecnyProjekt_KatalogLatek/Program.cs
+++ ZaverecnyProjekt_KatalogLatek/Program.cs
@@ -1,10 +1,64 @@
-﻿namespace ZaverecnyProjekt_KatalogLatek
+﻿namespace ZaverecnyProjekt_KatalogLatek.KatalogLatek
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Tady bude vznikat můj závěrený projekt z C# 2 - Czechitas. :)");
+            KatalogLatek katalog = new KatalogLatek();
+
+            // Počáteční přidání látek
+            katalog.PridejLatku(new Softshell(1, "Softshell červený", "červená", "jarní softshell", "100% polyester", 260, 199, 10, true, 8000));
+            katalog.PridejLatku(new BavlnenePlatno(2, "Plátno s pruhy", "bílá", "popelín", "100% bavlna", 120, 99, 5, false, 3));
+            katalog.PridejLatku(new Uplet(3, "Úplet s jednorožci", "vícebarvná", "úplet", "95% bavlna, 5% elastan", 160, 199, 12, true, 5));
+
+            // Uživatelské rozhraní
+            bool ukoncitProgram = false;
+            while (!ukoncitProgram)
+            {
+                ZobrazUzivatelskeRozhrani();
+                if (!int.TryParse(Console.ReadLine(), out int volba))
+                {
+                    Console.WriteLine("Neplatný výběr, zadej volbu znovu: ");
+                }
+
+                switch (volba)
+                {
+                    //case 1:
+                    //    katalog.PridejLatku(); // plátno (zatím neimplementováno)
+                    //    break;
+                    //case 2:
+                    //    katalog.PridejLatku(); // softshell (zatím neimplementováno)
+                    //    break;
+                    //case 3:
+                    //    katalog.PridejLatku(); // úplet (zatím neimplementováno)
+                    //    break;
+                    case 4:
+                        katalog.VypisInformaceOLatce();
+                        break;
+                    case 5: // potřebuje doladit
+                        bool platnyKodProduktu = int.TryParse(Console.ReadLine(), out int kodProduktu);
+                        katalog.OdeberLatku(kodProduktu);
+                        break;
+                    case 6:
+                        ukoncitProgram = true;
+                        break;
+                    case 7:
+                        Console.WriteLine("Neplatný výběr, zadej volbu znovu: ");
+                        break;
+                }
+            }
+        }
+
+        public static void ZobrazUzivatelskeRozhrani()
+        {
+            Console.WriteLine("Katalog látek - zvolte jednu z následujících možností:");
+            Console.WriteLine();
+            Console.WriteLine("1. Přidej bavlněné plátno");
+            Console.WriteLine("2. Přidej softshell");
+            Console.WriteLine("3. Přidej úplet");
+            Console.WriteLine("4. Vypiš všechny látky");
+            Console.WriteLine("5. Odeber látku z katalogu podle ID látky");
+            Console.WriteLine("6. Ukonči program");
         }
     }
 }
